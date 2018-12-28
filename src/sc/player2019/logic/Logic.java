f444cc8511916f48d437fe02fc931adf3f73c912
NRossmann <NRossmann@gmx.de>
2018-12-28 16:36:48 +0100

    Add first Logic

--- src/sc/player2019/logic/Logic.java
+++ src/sc/player2019/logic/Logic.java
@@ -8,6 +8,7 @@ import sc.plugin2019.*;
 import sc.plugin2019.util.GameRuleLogic;
 import sc.shared.GameResult;
 import sc.shared.PlayerColor;
+import sc.shared.WinCondition;
 
 import java.util.ArrayList;
 import java.util.Set;
@@ -51,6 +52,7 @@ public class Logic implements IGameHandler {
     log.info("Das Spiel ist beendet.");
     log.info("Error Message: "+errorMessage);
     log.info("Winner: "+data.getWinners());
+    log.info(currentPlayer.getColor().toString());
 
   }
 
@@ -60,51 +62,17 @@ public class Logic implements IGameHandler {
   @Override
   public void onRequestAction() {
     long startTime = System.currentTimeMillis();
-    int move = gameState.getTurn();
     PlayerColor mycolor = currentPlayer.getColor();
     log.info("Es wurde ein Zug angefordert.");
     ArrayList<Move> possibleMoves = GameRuleLogic.getPossibleMoves(gameState);
 
 
-
-    //print Field
-    int[][]field = getField();
-    for (int i = 0;i<=9 ; i++) {
-      int s = 9-i;
-        System.out.print(ANSI_CYAN+s+ANSI_RESET);
-      for (int j = 0; j<=9;j++){
-        if (field[j][s]==0) {
-          System.out.print(field[j][s]);
-        } else if (field[j][s]==1) {
-          System.out.print(ANSI_RED+field[j][s]+ANSI_RESET);
-        } else if (field[j][s]==2) {
-          System.out.print(ANSI_BLUE+field[j][s]+ANSI_RESET);
-        } else if (field[j][s]==3) {
-          System.out.print(ANSI_GREEN+field[j][s]+ANSI_RESET);
-        }
-      }
-      System.out.print("\n");
-    }
-    System.out.print(" ");
-    for (int k = 0; k<10;k++){
-        System.out.print(ANSI_CYAN+k+ANSI_RESET);
-    }
-    System.out.print("\n");
-    System.out.print("\n");
+    //Spielfeld ausgeben
+    int[][]field = getBoard(board);
+    printarray(field);
 
 
-    if (mycolor ==  PlayerColor.RED){
-        switch (move){
-            default:
-                sendAction(getsendmove(possibleMoves));break;
-        }
-    }else {
-        switch (move){
-
-            default:
-                sendAction(getsendmove(possibleMoves));break;
-        }
-    }
+    sendAction(getsendmove(possibleMoves));
 
 
 
@@ -127,6 +95,7 @@ public class Logic implements IGameHandler {
   @Override
   public void onUpdate(GameState gameState) {
     this.gameState = gameState;
+
     currentPlayer = gameState.getCurrentPlayer();
     board = gameState.getBoard();
     log.info("Zug: {} Spieler: {}", gameState.getTurn(), currentPlayer.getColor());
@@ -136,14 +105,16 @@ public class Logic implements IGameHandler {
    * {@inheritDoc}
    */
   @Override
-  public void sendAction(Move move) {client.sendMove(move);log.error(move.toString());}
+  public void sendAction(Move move) {client.sendMove(move);log.info(move.toString());}
 
-   int[][] getField(){
+
+  //Gibt Spielfeld als int Array zurück
+   int[][] getBoard(Board useboard){
     try {
       int[][] field = new int[10][10];
       for (int i = 0; i <= 9; i++) {
         for (int j = 0; j <= 9; j++) {
-          FieldState f = board.getField(i,j).getState();
+          FieldState f = useboard.getField(i,j).getState();
           if (f == FieldState.EMPTY) {
             field[i][j] = 0;
           } else if (f == FieldState.RED) {
@@ -164,6 +135,7 @@ public class Logic implements IGameHandler {
 
   }
 
+  //Zu getMovetoSwarm
   private ArrayList<Move> getMovetoField(Field field){
     ArrayList<Move> returnmoves = new ArrayList<>();
     Field[] ownFields = GameRuleLogic.getOwnFields(board,currentPlayer.getColor()).toArray(new Field[15]);
@@ -208,7 +180,7 @@ public class Logic implements IGameHandler {
     }
 
 
-
+  //Zu getMovetoSwarm
   private Boolean Fieldcheck(int x, int y, Direction direction, Field field){
     try{
       int distance = GameRuleLogic.calculateMoveDistance(board, x, y, direction);
@@ -222,6 +194,7 @@ public class Logic implements IGameHandler {
     return false;
   }
 
+  //Gibt Züge zurück die zu dem eigenen größten Schwarm führen
   private ArrayList<Move> getMovetoSwarm(Player player){
     Set<Field> swarmset = GameRuleLogic.greatestSwarm(board,player.getColor());
     Field[] swarm = swarmset.toArray(new Field[0]);
@@ -255,18 +228,178 @@ public class Logic implements IGameHandler {
   }
 
 
-
+  //Gibt den Zug zurück der gemacht werden soll
   private Move getsendmove(ArrayList<Move> possibleMoves){
-    ArrayList<Move> movess = getMovetoSwarm(currentPlayer);
-    if(!movess.isEmpty()){
-      log.debug("Logic Move");
-      return movess.get((int)(Math.random()*movess.size()));
+    int[] moveints = new int[possibleMoves.size()];
+    for (int i = 0; i<possibleMoves.size();i++){
+      moveints[i]=evaluateMove(possibleMoves.get(i));
+    }
+    int bestmove = search(moveints,moveints.length);
+
+    return possibleMoves.get(bestmove);
+
+  }
+
+
+
+
+
+    // Weist jedem Zug einen Wert je nach seiner Nützlichkeit zu
+  private int evaluateMove(Move move){
+    //Neues Game State Objekt
+    GameState gameState1 = gameState.clone();
+    //Rückgabe Variable
+    int moveint = 0;
+
+    //Schwarm größen vor dem Zug
+    int swarmbefore = GameRuleLogic.greatestSwarmSize(gameState1.getBoard(),currentPlayer.getColor());
+    int opswarmbefore = GameRuleLogic.greatestSwarmSize(gameState1.getBoard(),gameState.getOtherPlayerColor());
+
+    //Zug hypothetisch durchführen
+    gameState1 = domove(move);
+
+    //Prüfen ob jemand gewonnen hat
+    WinCondition winCondition = checkWinCondition(gameState1);
+    if (winCondition!=null){
+      PlayerColor winnercolor = winCondition.getWinner();
+      if (winnercolor == currentPlayer.getColor()){
+        return 1000;
+      }else {
+        return -1000;
+      }
+    }
+
+    //Schwarm größen nach dem Zug
+    int swarmafter = GameRuleLogic.greatestSwarmSize(gameState1.getBoard(),currentPlayer.getColor());
+    int opswarmafter = GameRuleLogic.greatestSwarmSize(gameState1.getBoard(),currentPlayer.getColor());
+
+    //Prüfen ob sich mein Schwarm vergößert hat
+    if (swarmafter>swarmbefore){
+      moveint += ((swarmafter-swarmbefore)*(swarmafter-swarmbefore))*2;
+    }else{
+      moveint -= ((swarmafter-swarmbefore)*(swarmafter-swarmbefore))*2;
+    }
+
+    //Prüfen ob sich der Schwarm des Gegners vergrößert hat
+    if (opswarmafter>opswarmbefore){
+      moveint -= ((opswarmafter-opswarmbefore)*(opswarmafter-opswarmbefore));
     }else {
-      log.debug("random Move");
-      return possibleMoves.get((int) (Math.random() * possibleMoves.size()));
+      moveint += ((opswarmafter-opswarmbefore)*(opswarmafter-opswarmbefore));
+    }
+
+    return moveint;
+  }
+
+  //Zug simuliert durchführen
+  private GameState domove(Move move){
+    GameState gameState1 = gameState.clone();
+    try {
+      move.perform(gameState1);
+    }catch (Exception e){
+      e.printStackTrace();
+    }
+    return gameState1;
+  }
+
+
+
+  private void printarray(int[][] field){
+    for (int i = 0;i<=9 ; i++) {
+      int s = 9-i;
+      System.out.print(ANSI_CYAN+s+ANSI_RESET);
+      for (int j = 0; j<=9;j++){
+        if (field[j][s]==0) {
+          System.out.print(field[j][s]);
+        } else if (field[j][s]==1) {
+          System.out.print(ANSI_RED+field[j][s]+ANSI_RESET);
+        } else if (field[j][s]==2) {
+          System.out.print(ANSI_BLUE+field[j][s]+ANSI_RESET);
+        } else if (field[j][s]==3) {
+          System.out.print(ANSI_GREEN+field[j][s]+ANSI_RESET);
+        }
+      }
+      System.out.print("\n");
+    }
+    System.out.print(" ");
+    for (int k = 0; k<10;k++){
+      System.out.print(ANSI_CYAN+k+ANSI_RESET);
+    }
+    System.out.print("\n");
+    System.out.print("\n");
+  }
+
+
+  //Testen ob jemand Gewonnen hat in hypothetischem GameState
+  public WinCondition checkWinCondition(GameState gameState1) {
+    int[][] stats = gameState1.getGameStats();
+    if (gameState1.getTurn() % 2 == 1) {
+      return null;
+    } else {
+      Player winningPlayer;
+      if (gameState1.getTurn() < 60) {
+        winningPlayer = this.getWinner(gameState1);
+        return winningPlayer != null ? new WinCondition(winningPlayer.getColor(), "Das Spiel ist beendet.\nEin Spieler hat seinen Schwarm vereint.") : null;
+      } else {
+        winningPlayer = this.getWinner(gameState1);
+        if (winningPlayer != null) {
+          return new WinCondition(winningPlayer.getColor(), "Das Spiel ist beendet.\nEin Spieler hat seinen Schwarm vereint.");
+        } else {
+          PlayerColor winner;
+          if (stats[0][0] > stats[1][0]) {
+            winner = PlayerColor.RED;
+          } else if (stats[0][0] < stats[1][0]) {
+            winner = PlayerColor.BLUE;
+          } else {
+            winner = null;
+          }
+
+          return new WinCondition(winner, "Das Rundenlimit wurde erreicht.");
+        }
+      }
     }
   }
 
+
+  private Player getWinner(GameState gameState1) {
+    if (GameRuleLogic.isSwarmConnected(gameState1.getBoard(), PlayerColor.RED)) {
+
+      if (GameRuleLogic.isSwarmConnected(gameState1.getBoard(), PlayerColor.BLUE)) {
+
+        if (gameState1.getPointsForPlayer(PlayerColor.RED) > gameState1.getPointsForPlayer(PlayerColor.BLUE)) {
+          return gameState1.getPlayer(PlayerColor.RED);
+        } else if (gameState1.getPointsForPlayer(PlayerColor.RED) < gameState1.getPointsForPlayer(PlayerColor.BLUE)) {
+          return gameState1.getPlayer(PlayerColor.BLUE);
+        } else {
+
+          return null;
+        }
+      } else {
+        return gameState1.getPlayer(PlayerColor.RED);
+      }
+    } else {
+
+      if (GameRuleLogic.isSwarmConnected(gameState1.getBoard(), PlayerColor.BLUE)) {
+
+        return gameState1.getPlayer(PlayerColor.BLUE);
+      } else {
+
+        return null;
+      }
+    }
+  }
+
+  //Suche nach dem höchsten Wert
+  private int search(int arr[], int n)
+  {
+    int s = 0;
+    for (int i = 0; i < n; i++) {
+
+      if (arr[i] > s)
+        s = arr[i];
+    }
+
+    return s;
+  }
 }
 
 
